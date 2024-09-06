ba6c6bd9753f2e238ae51a53ec4a34a0c8c3f1e4
Danielrks <91488329+Danielrks@users.noreply.github.com>
2024-09-06 21:10:52 +0300

    Player_Goblin_Tileset
    Added Player logic for attack hurt death jump fall attack and movement 1 2 3 and added goblin enemy that follows the player and attacks it and added a tileset node

---
+++ Scripts/Goblin.gd
@@ -0,0 +1,186 @@
+extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+@onready var detection_area = $Detection_Areas
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -250.0
+var Health = 30
+
+var target_position = Vector2(0, 0)
+var player = null
+var chase = false
+var attack_range = false
+var player_vulnerable = null
+var attacking = false
+var attacked = false
+var hurt = false
+#func _ready():
+	#collision_layer = 1 << 2  # "Enemies" layer is the 3rd layer
+	#collision_mask = ~(1 << 2)  # Invert the mask to exclude the "Enemies" layer
+	
+func _physics_process(delta: float) -> void:
+	# Add gravity
+
+	velocity += get_gravity() * delta
+	attacking = false
+	attacked = false
+	if chase:
+		target_position = player.position
+		var direction = target_position - global_position
+		direction = direction.normalized()
+		
+		"""if in_range:
+			target_position = player.position - Vector2(11, 0)
+			getting_attacked = player_damage.return_attacking()
+			if getting_attacked:
+				pass
+		if vulnerable_area:
+			if not is_attacking:
+				sprite.play("idle")
+			velocity.x = 0
+			timer_3.start()"""
+			
+		
+			#target_position = player.position - Vector2(11, 0)
+
+		sprite.flip_h = direction.x > 0
+			
+			
+			
+		
+			#target_position = player.position + Vector2(11, 0)
+		sprite.flip_h = direction.x < 0
+			
+		
+		velocity.x = direction.x * SPEED
+		
+		sprite.play("walking")
+	
+		
+		
+	elif attack_range:
+		target_position = player_vulnerable.position
+		if player_vulnerable.has_method("ReturnAttacking"):
+			attacked = player_vulnerable.ReturnAttacking()
+		var direction = target_position - global_position
+		direction = direction.normalized()
+		if direction.x > 0:
+			target_position = player_vulnerable.position - Vector2(11, 0)
+			sprite.flip_h = false
+			
+			
+			
+		elif direction.x < 0:
+			target_position = player_vulnerable.position + Vector2(11, 0)
+			sprite.flip_h = true
+		if Health <= 0:
+			Health = 0
+			sprite.play("death")
+			
+		elif attacked:
+			sprite.play("hurt")
+			Health -= 10
+			print(Health, "goblin")
+			hurt = true
+		if not attacked and not hurt:
+			sprite.play("attack_one")
+		
+		velocity.x = 0
+		
+	
+	else:
+		velocity.x = 0
+		sprite.play("idle")
+	
+		
+
+	# Apply movement
+	move_and_slide()
+
+
+
+	
+	
+
+
+
+	
+
+	
+	
+
+
+	
+
+func return_attacking():
+	return attacking
+
+	
+
+
+
+	
+
+
+func _on_detection_area_1_body_entered(body: Node2D) -> void:
+	player = body
+	print(body)
+	chase = true
+	print("entered chase range")
+	print(body)
+
+
+func _on_detection_area_1_body_exited(body: Node2D) -> void:
+	player = null
+	chase = false
+	print("left chase range")
+
+
+func _on_detection_area_2_body_entered(body: Node2D) -> void:
+	player = body
+	print(body)
+	chase = true
+	print("entered chase range")
+	print(body)
+
+
+func _on_detection_area_2_body_exited(body: Node2D) -> void:
+	player = null
+	chase = false
+	print("left chase range")
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	player_vulnerable = body
+	attack_range = true
+	print("entered attack range")
+	print(body)
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	player_vulnerable = null
+	attack_range = false
+	print("left attack range")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "attack_one":
+		attacking = true
+		print("finished attacking player", attacking)
+	if sprite.animation == "hurt":
+		attacked = false
+		hurt = false
+	if sprite.animation == "death":
+		queue_free()
+
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	if sprite.animation == "attack_one":
+		attacking = true
+		print("finished attacking player", attacking)
+	if sprite.animation == "hurt":
+		attacked = false
+		hurt = false
+	if sprite.animation == "death":
+		queue_free()
---
+++ Scripts/Player.gd
@@ -0,0 +1,149 @@
+extends CharacterBody2D
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -250.0
+@onready var sprite = $AnimatedSprite2D
+var current_animation := "idle"
+var Health = 100
+var Mana = 100
+var A1_Damage = 5
+var A2_Damage = 15
+var attacked = false
+var enemies = []
+var attacked_range = false
+var attacking = false
+func _physics_process(delta: float) -> void:
+	sprite.play(current_animation)
+	print(attacked)
+	attacked = false
+	attacking = false
+	print("Player Health: ", Health)
+	print("Player Mana: ", Mana)
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if attacked_range:
+		for enemy in enemies:
+			if enemy.has_method("return_attacking"):
+				attacked = enemy.return_attacking()
+				print("dan", enemy.attacking)
+			if attacked:
+				Health -= 5
+				if Health <= 0:
+					Health = 0
+					print(Health)
+					current_animation = "Death"
+				else:
+					current_animation = "hurt"
+					print(Health)
+		#
+	if Input.is_action_just_pressed("ui_left") and Input.is_action_just_pressed("ui_right"):
+		current_animation = "idle"
+	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
+		current_animation = "idle"
+	if Input.is_action_just_released("ui_left"):
+		current_animation = "idle"
+	if Input.is_action_just_released("ui_right"):
+		current_animation = "idle"
+		
+	if Input.is_action_pressed("ui_left") and not  Input.is_action_pressed("ui_right"):
+		sprite.flip_h = true
+		current_animation = "walking"
+		
+	if Input.is_action_pressed("ui_right") and not  Input.is_action_pressed("ui_left"):
+		sprite.flip_h = false
+		current_animation = "walking"
+	
+	if velocity.y > 0:
+		current_animation = "fall"
+		
+	
+			
+
+		
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		current_animation = "jump"
+	if Input.is_action_just_pressed("Left_Click") and not Input.is_action_just_pressed("ui_left") and not Input.is_action_pressed("ui_right") and current_animation != "fall": #and current_animation != "damaged":
+		current_animation = "attack_one"
+		attacking = true
+	#if Input.is_action_just_released("Left_Click") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right") and current_animation != "fall":
+		#current_animation = "idle"
+		#ttacking = false
+	if Input.is_action_just_pressed("Right_Click") and not Input.is_action_just_pressed("ui_left") and not Input.is_action_pressed("ui_right") and current_animation != "fall": #and current_animation != "damaged":
+		attacking = true
+		current_animation = "attack_two"
+	#if Input.is_action_just_released("Right_Click") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right") and current_animation != "fall":
+		#current_animation = "idle"
+		#attacking = false
+	if Input.is_action_just_pressed("Scroll_Wheel") and not Input.is_action_just_pressed("ui_left") and not Input.is_action_pressed("ui_right") and current_animation != "fall": #and current_animation != "damaged":
+		attacking = true
+		current_animation = "attack_three"
+	#if Input.is_action_just_released("Scroll_Wheel") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right") and current_animation != "fall":
+		#current_animation = "idle"
+		#attacking = false
+	
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+func return_health():
+	return Health
+
+
+
+	
+	#print("entered")
+	
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	#if current_animation == "attack_one":
+		#attacking = true
+		#print("player attacking")
+	if current_animation == "hurt":
+		current_animation = "idle"
+	if current_animation == "jump":
+		current_animation =  "fall"
+	if current_animation == "attack_one" or "attack_two" or "attack_three":
+		current_animation = "idle"
+	if current_animation == "idle" or "walking" or "jump" or "fall":
+		if Mana != 100:
+			Mana += 1
+	
+func _on_animated_sprite_2d_animation_looped() -> void:
+	#if current_animation == "attack_one":
+		#attacking = true
+	if current_animation == "hurt":
+		current_animation = "idle"
+	if current_animation == "jump":
+		current_animation = "fall"
+	if current_animation == "attack_one" or "attack_two" or "attack_three":
+		current_animation = "idle"
+	if current_animation == "idle" or "walking" or "jump" or "fall":
+		if Mana != 100:
+			Mana += 1
+	if is_on_floor():
+		current_animation = "idle"
+func ReturnAttacking():
+	return attacking
+
+
+func _on_attack_attacked_area_body_entered(body: Node2D) -> void:
+	enemies.append(body)
+	attacked_range = true	
+	print("player_area", enemies)
+
+
+
+func _on_attack_attacked_area_body_exited(body: Node2D) -> void:
+	enemies = []
+	attacked_range = false	
